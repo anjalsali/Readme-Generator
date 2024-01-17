79d54db575b9eff48fbb4c5b34a846e5c21eaa53
Anjal Sali <anjalsali55@gmail.com>
2024-01-17 03:45:13 +0000

    implemented generateMarkdown funciton to create readme.md file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,42 @@
-// function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+   return `
+# ${data.title}
 
-`;
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Description
+${data.description}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+This project is licensed under the ${data.license} license.
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+For any questions, please contact me:
+
+GitHub: [${data.github}](https://github.com/${data.github})
+
+Email: ${data.email}
+  `;
 }
 
 module.exports = generateMarkdown;
